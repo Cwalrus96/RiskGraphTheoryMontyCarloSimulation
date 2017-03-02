680d9ee414dce600a6e83a1451da05ed1e1bfc01
Cwalrus96 <Cwalrus96@OBAMA>
2017-03-02 15:49:36 -0600

    Started Classes

---
+++ ConsoleApplication1/ConsoleApplication1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Node
+    {
+        int count = 0; 
+        string label;
+        List<Edge> edges;
+
+        public Node(string label)
+        {
+            this.label = label;
+            edges = new List<Edge>();
+        }
+
+        public Node RandomTravel()
+        {
+            Random r = new Random();
+            return edges[Math.Round((double)(r.Next(edges.Count()))];
+
+        }
+    }
+}
